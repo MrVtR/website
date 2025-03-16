8ec24842a487bbfbc2cdfa416efdb53cceb51c57
MrVtR <vitor.ribeiro0803@gmail.com>
2025-03-15 21:40:47 -0300

    Update

--- app/(website)/author/[author]/author.js
+++ app/(website)/author/[author]/author.js
@@ -17,29 +17,44 @@ export default function Author(props) {
   return (
     <>
       <Container>
-        <div className="flex flex-col items-center justify-center">
-          <div className="relative h-20 w-20 overflow-hidden rounded-full">
-            {author?.image && (
-              <Image
-                src={urlForImage(author.image).src}
-                alt={author.name || " "}
-                fill
-                sizes="(max-width: 320px) 100vw, 320px"
-                className="object-cover"
+        <div
+          style={{
+            backgroundColor: "rgb(0, 0, 0, 0.1)",
+            marginTop: "32px",
+            padding: "0 10px 20px 10px",
+            borderRadius: "15px",
+          }}
+        >
+          <div className="flex flex-col items-center justify-center pt-[20px]">
+            <div className="relative h-20 w-20 overflow-hidden rounded-full">
+              {author?.image && (
+                <Image
+                  src={urlForImage(author.image).src}
+                  alt={author.name || " "}
+                  fill
+                  sizes="(max-width: 320px) 100vw, 320px"
+                  className="object-cover"
+                />
+              )}
+            </div>
+            <h1 className="text-brand-primary mt-2 text-3xl font-semibold tracking-tight dark:text-white lg:text-3xl lg:leading-tight">
+              {author.name}
+            </h1>
+            <div className="mx-auto mt-2 flex max-w-xl flex-col px-5 text-center text-gray-500">
+              {author.bio && <PortableText value={author.bio} />}
+            </div>
+          </div>
+
+          <div className="align-stretch mt-3 flex grid  md:grid-cols-2 lg:gap-[10px] xl:grid-cols-3">
+            {posts.map((post) => (
+              <PostList
+                key={post._id}
+                post={post}
+                aspect="landscape"
+                maxHeight="190px"
               />
-            )}
+            ))}
           </div>
-          <h1 className="text-brand-primary mt-2 text-3xl font-semibold tracking-tight dark:text-white lg:text-3xl lg:leading-tight">
-            {author.name}
-          </h1>
-          <div className="mx-auto mt-2 flex max-w-xl flex-col px-5 text-center text-gray-500">
-            {author.bio && <PortableText value={author.bio} />}
-          </div>
-        </div>
-        <div className="mt-16 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
-          {posts.map(post => (
-            <PostList key={post._id} post={post} aspect="square" />
-          ))}
         </div>
       </Container>
     </>
